7b8e180019c4db4ef7e30f4678f4b5da6e528ebd
CUONG Pham Van (VSL.Delivery) <cuong.phamvan@vti.vn>
2024-06-27 11:08:48 +0700

    add

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,22 +1,7 @@
 
-import { Routes, Route,  BrowserRouter } from 'react-router-dom';
-import routes from './routes';
 import Home from './pages/Home/Home';
-
-
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     {
-    //       routes.map((route,index)=>{
-    //         const Page = route.element;
-    //         return <Route  key={index} path={route.path} element={<Page/>} />
-    //       })
-    //     }
-    //   </Routes>
-    // </BrowserRouter>
-
     <Home/>
   );
 }
